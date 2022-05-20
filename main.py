bb031df1842f1d562c2d9210a6c0edfb18f95194
Manchere <manucheremeh1995@gmail.com>
2022-05-20 03:54:26 +0200

    add: flexible estimate

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
 import maya.cmds as cmds
 
+
 def createJoint():
     j_number = cmds.intField(jointNumber, q=True, v=True)
-    parentJ = ''
+    parentJnt = ''
+    rootJnt = ''
     curveSelected = cmds.ls(sl=True)[0]
-    rootJnt =''
+    rad = cmds.radioCollection(orient, q=True, sl=True)
+
+    # flexible estimate
+    curveLen = cmds.arclen(curveSelected)
+    if cmds.checkBox('flexEst', q=True, v=True): j_number = int(round(curveLen / 0.27))
 
     for i in range(j_number):
         cmds.select(cl=True)
-        newJ = cmds.joint()
-        mp = cmds.pathAnimation(newJ, c=curveSelected, fm=True)
+        newJnt = cmds.joint()
+        mp = cmds.pathAnimation(newJnt, c=curveSelected, fm=True)
         cmds.cutKey(mp + '.u', time=())
         cmds.setAttr(mp + '.u', i * (1.0 / (j_number - 1)))
-        cmds.delete(newJ+'.tx', icn=True)
-        cmds.delete(newJ+'.ty', icn=True)
-        cmds.delete(newJ+'.tz', icn=True)
+        cmds.delete(newJnt + '.tx', icn=True)
+        cmds.delete(newJnt + '.ty', icn=True)
+        cmds.delete(newJnt + '.tz', icn=True)
         cmds.delete(mp)
 
         if i == 0:
-            parentJ = newJ
-            cmds.joint(newJ,e=True, oj = 'xyz', sao=)
+            parentJnt = newJnt
+            rootJnt = newJnt
             continue
 
-        cmds.parent(newJ, parentJ)
-        parentJ = newJ
+        cmds.parent(newJnt, parentJnt)
+        parentJnt = newJnt
+        if rad == 'child': cmds.joint(rootJnt, edit=True, oj='xyz', sao='yup', ch=True, zso=True)
 
 
 if cmds.window('joint_Curve', exists=True):
@@ -31,10 +38,16 @@ if cmds.window('joint_Curve', exists=True):
 
 cmds.window('joint_Curve', title='Joint Along Curve', w=150)
 cmds.columnLayout(adjustableColumn=True)
+
 cmds.frameLayout(label='Select a curve', cll=False, bgc=[0.3, 0, 0.3], w=200)
-jointNumber = cmds.intField(v=5, min=2)
+jointNumber = cmds.intField(v=15, min=2)
 cmds.checkBox('flexEst', label='Flexible estimate')
+
 cmds.frameLayout(label='Orientation', cll=False, bgc=[0.3, 0, 0.3], w=200)
-cmds.radioCollection()
+orient = cmds.radioCollection()
+cmds.radioButton('world', label='World orientation', sl=True)
+cmds.radioButton('child', label='Child orientation')
+
 cmds.button('create', c='createJoint()')
+
 cmds.showWindow()
